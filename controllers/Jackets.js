f82e2d37cbd09b34939133bd6b2a810706489a33
BHANU PRAKASH BATHINI <S566650@nwmissouri.edu>
2023-11-15 20:09:37 -0600

    second part

--- controllers/Jackets.js
+++ controllers/Jackets.js
@@ -146,3 +146,17 @@ exports.jackets_update_Page = async function(req, res) {
     res.send(`{'error': '${err}'}`);
     }
    };
+
+
+   // Handle a delete one view with id from query
+exports.jackets_delete_Page = async function(req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try{
+    result = await Jackets.findById(req.query.id)
+    res.render('jacketsdelete', { title: 'Jackets Delete', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
