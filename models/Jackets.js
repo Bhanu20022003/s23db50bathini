dea58aa4ab879cb8aecc465467387cea927d9707
BHANU PRAKASH BATHINI <S566650@nwmissouri.edu>
2023-12-01 01:29:50 -0600

    second part

--- models/Jackets.js
+++ models/Jackets.js
@@ -1,6 +1,15 @@
 const mongoose = require("mongoose")
 const jacketSchema = mongoose.Schema({
-jackets: String,
+    jackets: {
+        type: String,
+        validate: {
+          validator: function (value) {
+            return /^[a-zA-Z]+$/.test(value);
+          },
+          message: 'Jacket name  should contain only alphabets'
+        },
+        required: [true, 'jacket is required']
+      },
 cost: Number,
 brand:  String
 })
