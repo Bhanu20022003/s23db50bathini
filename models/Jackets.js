f99fe163f5e25670a2c2d7511e00e38bf3eb3bef
BHANU PRAKASH BATHINI <S566650@nwmissouri.edu>
2023-12-01 01:30:35 -0600

    second part

--- models/Jackets.js
+++ models/Jackets.js
@@ -10,7 +10,21 @@ const jacketSchema = mongoose.Schema({
         },
         required: [true, 'jacket is required']
       },
-cost: Number,
-brand:  String
+      cost: {
+        type: Number,
+        min: [1, 'Costmust be greater than 0'],
+        max: [100, 'Cost cannot exceed 100'],
+        required: [true, 'Cost is required']
+      },
+      brand: {
+        type: String,
+        validate: {
+          validator: function (value) {
+            return /^[a-zA-Z]+$/.test(value);
+          },
+          message: 'Brand name  should contain only alphabets'
+        },
+        required: [true, 'jacket is required']
+      }
 })
 module.exports = mongoose.model("Jackets",jacketSchema)
